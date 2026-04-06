6e02fc6282a048cc2e5455dbfb215fcd10346932
Gloria Jaramillo <endermitas3unidas@gmail.com>
2026-04-06 10:44:18 -0500

    deploy backend azure

--- src/api/src/main.ts
+++ src/api/src/main.ts
@@ -5,27 +5,30 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Habilitar CORS
+  // ✅ Habilitar CORS para frontend (Azure o local)
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  // ✅ Configuración de Swagger
+  // 🌐 Swagger configuración
   const config = new DocumentBuilder()
-  .setTitle('OrderHub API')
-  .setDescription('API base para la Semana 1 de AZ-204')
-  .setVersion('1.0')
-  .addServer('http://localhost:3000') // 👈 AGREGA ESTA LÍNEA
-  .build();
+    .setTitle('OrderHub API')
+    .setDescription('API base para la Semana 1 de AZ-204')
+    .setVersion('1.0')
+    .addServer(process.env.BASE_URL || 'http://localhost:3000')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('swagger', app, document);
 
+  // 🚀 Puerto dinámico (Azure usa process.env.PORT)
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  console.log(`🚀 API running on: http://localhost:${port}`);
 }
 
 bootstrap();
